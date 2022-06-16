68cf99934215e18bcd60912f1e4d37d53f4a4250
Adam Ridhwan <adamridhwan.1@gmail.com>
2022-06-16 19:26:23 -0400

    added-styled components
    I learned about migrating from regular scss to styled components. I also created custom buttons for base buttons, google sign and inverted.

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -11,7 +11,7 @@ import { CartContext } from '../../contexts/cart.context';
 
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
-import './navigation.styles.scss';
+import { NavigationContainer, NavLinks, NavLink, LogoContainer } from './navigation.styles.js';
 
 const Navigation = () => {
 	const { currentUser } = useContext(UserContext);
@@ -19,28 +19,27 @@ const Navigation = () => {
 
 	return (
 		<Fragment>
-			<div className='navigation'>
-
-				<Link className='logo-container' to='/'>
+			<NavigationContainer className='navigation'>
+				<LogoContainer to='/'>
 					<CrwnLogo className='logo' />
-				</Link>
+				</LogoContainer>
 
-				<div className='nav-links-container'>
-					<Link className='nav-link' to='/shop'>
+				<NavLinks>
+					<NavLink to='/shop'>
 						SHOP
-					</Link>
+					</NavLink>
 
 					{currentUser ? (
-						<span className='nav-link' onClick={signOutUser}>SIGN OUT</span>
+						<NavLink as='span' className='nav-link' onClick={signOutUser}>SIGN OUT</NavLink>
 					) : (
-						<Link className='nav-link' to='/auth'>
+						<NavLink to='/auth'>
 							SIGN IN
-						</Link>
+						</NavLink>
 					)}
 					<CartIcon />
-				</div>
+				</NavLinks>
 				{isCartOpen && <CartDropdown />}
-			</div>
+			</NavigationContainer>
 			<Outlet />
 		</Fragment>
 	);
